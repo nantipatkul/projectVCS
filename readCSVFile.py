7d9dc467a04e5eced843b84d64cf343f0cb50e64
Nantipat <nantipat.k@hotmail.com>
2019-12-18 23:35:49 +0700

    Version 4.0.0
    ADD Function showGrade

--- readCSVFile.py
+++ readCSVFile.py
@@ -4,17 +4,24 @@ def showSubjectCode(subjectCode):
     for code in subjectCode:
         print(code)
 
-def showCourseName(CoureName):
-    for name in CoureName:
+def showCourseName(coureName):
+    for name in coureName:
         print(name) 
+
+def showGrade(gradeList):
+    for grade in gradeList:
+        print(grade) 
 
 subjectcodelist =[]
 coursename = []
+gradelist =[]
 with open("semester3.csv") as filecsv:
     readCSV = csv.reader(filecsv)
     for row in readCSV:
         subjectcodelist.append(row[0])
         coursename.append(row[1])
+        gradelist.append(row[2])
 
 showSubjectCode(subjectcodelist)
 showCourseName(coursename)
+showGrade(gradelist)
