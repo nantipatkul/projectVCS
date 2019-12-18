5d3313d224a274a8e0ace9c6caa11dbdc278e1d5
Nantipat <nantipat.k@hotmail.com>
2019-12-18 23:26:51 +0700

    Version 3.0.0
    Edit filer readcsv edit fuction and script line 19-20

--- readCSVFile.py
+++ readCSVFile.py
@@ -1,9 +1,20 @@
 import csv
 
 def showSubjectCode(subjectCode):
-    print(subjectCode)
+    for code in subjectCode:
+        print(code)
 
+def showCourseName(CoureName):
+    for name in CoureName:
+        print(name) 
+
+subjectcodelist =[]
+coursename = []
 with open("semester3.csv") as filecsv:
     readCSV = csv.reader(filecsv)
     for row in readCSV:
-        showSubjectCode(row[0])
+        subjectcodelist.append(row[0])
+        coursename.append(row[1])
+
+showSubjectCode(subjectcodelist)
+showCourseName(coursename)
