37d0f3af4d9592bd3a4eafa20ac9bb72b1f1aeff
Nantipat <nantipat.k@hotmail.com>
2019-12-18 23:16:58 +0700

    Version 2.0.1
    Edit readCsvfile

--- readCSVFile.py
+++ readCSVFile.py
@@ -1,11 +1,9 @@
 import csv
 
 def showSubjectCode(subjectCode):
-    for code in subjectCode:
-        print(code)
-
+    print(subjectCode)
 
 with open("semester3.csv") as filecsv:
     readCSV = csv.reader(filecsv)
     for row in readCSV:
-        print(row)
+        showSubjectCode(row[0])
