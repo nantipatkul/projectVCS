7277a008922211b0faf28b90bf90c740bb07fcbb
Nantipat <nantipat.k@hotmail.com>
2019-12-19 16:45:56 +0700

    Revert "Version 3.0.0.1"
    This reverts commit 5d3313d224a274a8e0ace9c6caa11dbdc278e1d5.

--- readCSVFile.py
+++ readCSVFile.py
@@ -1,20 +1,9 @@
 import csv
 
 def showSubjectCode(subjectCode):
-    for code in subjectCode:
-        print(code)
+    print(subjectCode)
 
-def showCourseName(CoureName):
-    for name in CoureName:
-        print(name) 
-
-subjectcodelist =[]
-coursename = []
 with open("semester3.csv") as filecsv:
     readCSV = csv.reader(filecsv)
     for row in readCSV:
-        subjectcodelist.append(row[0])
-        coursename.append(row[1])
-
-showSubjectCode(subjectcodelist)
-showCourseName(coursename)
+        showSubjectCode(row[0])
