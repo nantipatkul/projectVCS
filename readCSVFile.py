e984974da20b0f63e275121b71ab784c33b072ef
Nantipat <nantipat.k@hotmail.com>
2019-12-19 21:42:48 +0700

    Ver 5.0.0

--- readCSVFile.py
+++ readCSVFile.py
@@ -4,6 +4,7 @@ def showCode(codelist):
     for data in codelist:
         print(data)
 
+<<<<<<< HEAD
 datalist = []
 with open("semester2.csv") as filecsv:
     readCSV = csv.reader(filecsv)
@@ -11,3 +12,19 @@ with open("semester2.csv") as filecsv:
         datalist.append(row[0])
 
 showCode(datalist)
+=======
+def showCourseName(CoureName):
+    for name in CoureName:
+        print(name) 
+
+subjectcodelist =[]
+coursename = []
+with open("semester3.csv") as filecsv:
+    readCSV = csv.reader(filecsv)
+    for row in readCSV:
+        subjectcodelist.append(row[0])
+        coursename.append(row[1])
+
+showSubjectCode(subjectcodelist)
+showCourseName(coursename)
+>>>>>>> parent of 7d9dc46... Version 4.0.0
