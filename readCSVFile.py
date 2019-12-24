8170655547e11f89b2488e93e7c7f15c18f16ebe
Nantipat <nantipat.k@hotmail.com>
2019-12-24 15:26:32 +0700

    new version

--- readCSVFile.py
+++ readCSVFile.py
@@ -15,7 +15,4 @@ data = pd.read_csv('semester3.csv')
 df = pd.DataFrame(data)
 print(df)
 
-with open('log/output.txt','w+') as target:
-    target.write(df.to_string(header = False ,index = False))
-os.startfile($(pwd)"/output.txt')
 
