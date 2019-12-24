96d1d8f44ee75b3b9eb523fb11106abd9c90df23
Nantipat <nantipat.k@hotmail.com>
2019-12-24 09:12:49 +0700

    YML

--- readCSVFile.py
+++ readCSVFile.py
@@ -4,15 +4,13 @@ def showCode(codelist):
     for data in codelist:
         print(data)
 
-def showCourseName(CoureName):
-    for name in CoureName:
-        print(name) 
+def showCourseName(CoureName):  
+    print(CoureName)
 
 subjectcodelist =[]
 coursename = []
 
-data = pd.read_csv('semester2.csv')
-
-print(data)
-
+data = pd.read_csv('semester3.csv')
+df = pd.DataFrame(data)
+print(df)
 
